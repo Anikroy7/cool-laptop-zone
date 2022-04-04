c2fb21827eafe1660a9a6505e3ad48bce4b6d56f
Anikroy7 <roy645800@gmail.com>
2022-04-04 16:33:57 +0600

    navbar added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './Navbar.css'
 const Navbar = () => {
     return (
-        <nav>
-
+        <nav className='navbar'>
+            <Link to={'/'}>Home</Link>
+            <Link to={'reviews'}>Reviews</Link>
+            <Link to={'dashboard'}>Dashboard</Link>
+            <Link to={'blogs'}>Blogs</Link>
+            <Link to={'about'}>About</Link>
         </nav>
     );
 };

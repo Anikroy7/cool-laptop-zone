d34a9f71b906c6019ee91feb0862636fda6ecf9d
Anikroy7 <roy645800@gmail.com>
2022-04-04 18:19:07 +0600

    display all data in ui

--- src/Components/ReviewDetails/ReviewDetails.js
+++ src/Components/ReviewDetails/ReviewDetails.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './ReviewDetails.css';
 const ReviewDetails = (props) => {
-    const { name, review } = props.review
+    const { name, review, ratings } = props.review
     return (
-        <div>
-            <h1>name: {name}</h1>
-            <p>review:</p>
-            <p>{review}</p>
+        <div className='bg-slate-300 p-10 rounded-lg m-2'>
+            <h1 className='text-3xl font-bold '>Name: {name}</h1>
+            <p className='mt-4 text-xl font-semibold'>Review:</p>
+            <p className='mt-8 text-xl font-semibold'>{review}</p>
+            <p className='mt-4 text-xl font-semibold'>Ratings: {ratings}</p>
         </div>
     );
 };

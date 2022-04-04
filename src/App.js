df387cc6ea010d05a31229753298d97f70b8c232
Anikroy7 <roy645800@gmail.com>
2022-04-04 16:45:30 +0600

    all routes declare for navbar link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import Dashboard from './Components/DashBoard/Dashboard';
+import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
+import Reviews from './Components/Reviews/Reviews';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+
+
+      <Routes>
+
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews />}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+
+      </Routes>
+
     </div>
   );
 }
---
+++ src/Components/Blogs/Blogs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h3>
+                This is blogs component
+            </h3>
+        </div>
+    );
+};
+
+export default Blogs;

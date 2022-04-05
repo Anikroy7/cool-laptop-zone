acc074dd5dc836bd7b035c82b45368e1f704f53c
Anikroy7 <roy645800@gmail.com>
2022-04-05 17:51:54 +0600

    image in home reviews ui

--- src/Components/ReviewDetails/ReviewDetails.js
+++ src/Components/ReviewDetails/ReviewDetails.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './ReviewDetails.css';
 const ReviewDetails = (props) => {
-    const { name, review, ratings } = props.review
+    const { name, review, ratings, image } = props.review
     return (
-        <div className='bg-slate-300 p-10 rounded-lg m-2'>
-            <h1 className='text-3xl font-bold '>Name: {name}</h1>
-            <p className='mt-4 text-xl font-semibold'>Review:</p>
-            <p className='mt-2 text-xl font-semibold'>{review}</p>
-            <p className='mt-4 text-xl font-semibold'>Ratings: {ratings}</p>
+        <div className='bg-slate-300 p-2 rounded-lg m-2'>
+            <div className='review-image-container mt-4'>
+                <img src={image} alt="" />
+                <h1 className='text-xl font-bold text-cyan-800'> {name}</h1>
+            </div>
+            <div className='review-details-container'>
+                <p className='mt-4 text-xl font-semibold'>Review:</p>
+                <p className='mt-2 '>{review}</p>
+                <p className='mt-4  font-semibold'>Ratings: {ratings}</p>
+            </div>
         </div>
     );
 };
--- src/Components/ShowAllReviews/ShowAllReviews.js
+++ src/Components/ShowAllReviews/ShowAllReviews.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const ShowAllReviews = (props) => {
-    const { name, ratings, review } = props.reviews;
+    const { name, ratings, review, image } = props.reviews;
     return (
-        <div className='bg-slate-300 p-10 rounded-lg m-2'>
-            <h1 className='text-3xl font-bold '>Name: {name}</h1>
-            <p className='mt-4 text-xl font-semibold'>Review:</p>
-            <p className='mt-2 text-xl font-semibold'>{review}</p>
-            <p className='mt-4 text-xl font-semibold'>Ratings: {ratings}</p>
+        <div className='bg-slate-300  rounded-lg m-2'>
+            <div >
+                <img src={image} alt="" />
+            </div>
+            <div>
+                <h1 className='text-3xl font-bold '>Name: {name}</h1>
+                <p className='mt-4 text-xl font-semibold'>Review:</p>
+                <p className='mt-2 text-xl font-semibold'>{review}</p>
+                <p className='mt-4 text-xl font-semibold'>Ratings: {ratings}</p>
+            </div>
         </div>
     );
 };

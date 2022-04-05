da7765f20ef70312ebbd0aaaa38de6e37fcc0264
Anikroy7 <roy645800@gmail.com>
2022-04-05 18:08:08 +0600

    show image in show all review component

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import useReviews from '../../hooks/useReviews';
 import ShowAllReviews from '../ShowAllReviews/ShowAllReviews'
+import './Reivews.css'
 
 const Reviews = () => {
     const [reviews, setReviews] = useReviews();
 
 
     return (
-        <div>
+        <div className='show-all-reviews-container'>
             {
                 reviews.map(review => <ShowAllReviews
                     reviews={review}
--- src/Components/ShowAllReviews/ShowAllReviews.js
+++ src/Components/ShowAllReviews/ShowAllReviews.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './ShowAllReviews.css'
 
 const ShowAllReviews = (props) => {
     const { name, ratings, review, image } = props.reviews;
     return (
-        <div className='bg-slate-300  rounded-lg m-2'>
-            <div >
+        <div className='bg-red-200  rounded-lg m-2 p-8'>
+            <div className='all-details-image-container text-center '>
                 <img src={image} alt="" />
             </div>
-            <div>
+            <div className='all-details-container mx-auto'>
                 <h1 className='text-3xl font-bold '>Name: {name}</h1>
                 <p className='mt-4 text-xl font-semibold'>Review:</p>
                 <p className='mt-2 text-xl font-semibold'>{review}</p>
